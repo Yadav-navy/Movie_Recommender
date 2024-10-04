f5f7615433c53173b2672d060ae1207c144a72ee
Yadav-navy <yadav.navy03@gmail.com>
2024-10-04 14:30:19 +0530

    Name_Changed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-st.title('Movie Recommendation System')
+st.title('Movie Advisor')
 
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
